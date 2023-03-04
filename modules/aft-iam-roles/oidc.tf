a75e0e60ac6171d44cff929a983f2fb5c6acf29a
wellsiau-aws <wellsiau@amazon.com>
2023-03-03 23:16:30 -0800

    fix local conditional

--- modules/aft-iam-roles/oidc.tf
+++ modules/aft-iam-roles/oidc.tf
@@ -1,10 +1,10 @@
 data "tls_certificate" "tfc_certificate" {
-  count = is_dynamic_provider_credentials ? 1 : 0
+  count = local.is_dynamic_provider_credentials ? 1 : 0
   url   = "https://${local.terraform_api_hostname}"
 }
 
 resource "aws_iam_openid_connect_provider" "tfc_provider" {
-  count           = is_dynamic_provider_credentials ? 1 : 0
+  count           = local.is_dynamic_provider_credentials ? 1 : 0
   url             = data.tls_certificate.tfc_certificate.url
   client_id_list  = [var.terraform_oidc_audience]
   thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
