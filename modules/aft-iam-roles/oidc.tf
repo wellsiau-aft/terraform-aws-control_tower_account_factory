d996739c844f4fe3c0fee894d1f9483c3372ce8f
wellsiau-aws <wellsiau@amazon.com>
2023-03-03 23:50:21 -0800

    fix target provider

--- modules/aft-iam-roles/oidc.tf
+++ modules/aft-iam-roles/oidc.tf
@@ -5,6 +5,7 @@ data "tls_certificate" "tfc_certificate" {
 
 resource "aws_iam_openid_connect_provider" "tfc_provider" {
   count           = local.is_dynamic_provider_credentials ? 1 : 0
+  provider        = aws.aft_management
   url             = data.tls_certificate.tfc_certificate[count.index].url
   client_id_list  = [var.terraform_oidc_audience]
   thumbprint_list = [data.tls_certificate.tfc_certificate[count.index].certificates[0].sha1_fingerprint]
